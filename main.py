19022fe264b1c5800d0b94cd025f428eef572880
Suryateja Duvvuri <35475392+SuryatejaDuvvuri@users.noreply.github.com>
2024-05-30 23:49:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ import firebase_admin
 from firebase_admin import credentials, db
 from PIL import Image
 import json
+from firebase_admin import db
 
 cred1 = credentials.Certificate("uplifted-env-424901-t2-firebase-adminsdk-1n3uy-07457c1d15.json")
 firebase_admin.initialize_app(cred1, {'databaseURL': "https://uplifted-env-424901-t2-default-rtdb.firebaseio.com/"})
 ref = db.reference('Inventory') 
 
 
-ref.update({
-    'Ragu': {
-        'name': 'Ragu',
-        'quantity': 10,
-        'image': 'Ragu.jpeg',
-        'Aisle': '1',
+# ref.update({
+#     'Ragu': {
+#         'name': 'Ragu',
+#         'Count': 10,
+#         'image': 'Ragu.jpeg',
+#         'Aisle': '1',
+#         'Missing': False,
+#         'Misplaced': False
+#     }
+# })
+
+#adding item
+data = {
+    'name': 'Kellogs Cereal Box',
+        'Count': 5,
+        'image': 'Cereal.jpeg',
+        'Aisle': '3',
         'Missing': False,
         'Misplaced': False
-    }
-})
+        }
+ref.child('Kellogs Cereal Box').set(data) 
+
+
 storage_client = storage.Client()  # No additional arguments needed
 
 # # Get a reference to a bucket
@@ -26,21 +40,24 @@ bucket_name = 'cs131-tests'
 bucket = storage_client.bucket(bucket_name)
 
 # List blobs (files) in the bucket
-for blob in bucket.list_blobs():
-    ref.child(blob).child('image').get()
     
 
-# # Download a blob
+# # Downloading and uploading images of grocery items
 # blob_name = 'Ragu.jpeg'
 # # blob = bucket.blob(blob_name)
 # # blob.download_to_filename('Ragu.jpeg')
 
 
 
-blob_name = "Ragu.jpeg"
-blob = bucket.blob(blob_name)
-blob.upload_from_filename(blob_name)
+# blob_name = "Ragu.jpeg"
+# blob = bucket.blob(blob_name)
+# blob.upload_from_filename(blob_name)
 
-
-
-
+# Updating inventory data
+for item in ref.get():
+   val = ref.child(item).child('Count').get()
+   if val == 0:
+       print("Item is out of stock")
+   else:
+       ref.child(item).update({'Count': val - 1}) 
+       print('Count: ', ref.child(item).child('Count').get())
